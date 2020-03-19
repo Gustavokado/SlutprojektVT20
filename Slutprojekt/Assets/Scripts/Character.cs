c7fe9216d262ea15b222cff37eec90f21f3ff571
Gustavokado <gusmel79@gmail.com>
2020-03-19 16:27:20 +0100

    basic movement(rotation funka inte)

---
+++ Slutprojekt/Assets/Scripts/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    protected Vector3 input;
+    public float speed;
+    Vector3 currentEulerAngles;
+    Quaternion currentRotation;
+
+    void Start()
+    {
+        
+    }
+
+
+    protected void Move(Vector3 movement)
+    {
+        transform.Translate(input*Time.deltaTime * speed);
+
+        currentEulerAngles = new Vector3(0, 0, Mathf.Asin(input.x / input.y) * Mathf.Rad2Deg);
+        currentRotation.eulerAngles = currentEulerAngles;
+        transform.rotation = currentRotation;
+    }
+
+    void Rotate(float angle)
+    {
+        currentEulerAngles += new Vector3(0, 0, Mathf.Asin(input.x / input.y) * Mathf.Rad2Deg);
+        currentRotation.eulerAngles = currentEulerAngles;
+        transform.rotation = currentRotation;
+    }
+
+}
---
+++ Slutprojekt/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Character
+{
+    
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        Move(input);
+        
+    }
+}
