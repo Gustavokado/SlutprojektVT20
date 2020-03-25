9075360abb5d51eb1c82084c2a8a050ecc617716
Gustavokado <gusmel79@gmail.com>
2020-03-25 14:56:14 +0100

    rotationsAreDumb

--- Slutprojekt/Assets/Scripts/Character.cs
+++ Slutprojekt/Assets/Scripts/Character.cs
@@ -6,29 +6,33 @@ public class Character : MonoBehaviour
 {
     protected Vector3 input;
     public float speed;
-    Vector3 currentEulerAngles;
-    Quaternion currentRotation;
+    public float currentRotation = 0f;
+    public float newRotation = 0f;
+    /*Vector3 currentEulerAngles;
+    Quaternion currentRotation;*/
 
     void Start()
     {
         
     }
-
-
+    
+    
     protected void Move(Vector3 movement)
     {
-        transform.Translate(input*Time.deltaTime * speed);
-
-        currentEulerAngles = new Vector3(0, 0, Mathf.Asin(input.x / input.y) * Mathf.Rad2Deg);
+        transform.Translate(movement*Time.deltaTime * speed);
+        newRotation = Mathf.Asin(movement.x / movement.y)*Mathf.Rad2Deg - currentRotation;
+        transform.Rotate(0f, 0f, newRotation, Space.World);
+        currentRotation = newRotation;
+        /*currentEulerAngles = new Vector3(0, 0, Mathf.Sin(movement.x / movement.y) * Mathf.Rad2Deg);
         currentRotation.eulerAngles = currentEulerAngles;
-        transform.rotation = currentRotation;
+        transform.rotation = currentRotation;*/
     }
 
     void Rotate(float angle)
     {
-        currentEulerAngles += new Vector3(0, 0, Mathf.Asin(input.x / input.y) * Mathf.Rad2Deg);
+        /*currentEulerAngles += new Vector3(0, 0, Mathf.Asin(input.x / input.y) * Mathf.Rad2Deg);
         currentRotation.eulerAngles = currentEulerAngles;
-        transform.rotation = currentRotation;
+        transform.rotation = currentRotation;*/
     }
 
 }
